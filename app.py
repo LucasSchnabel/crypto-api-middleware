ddff8f7dd29571d64fdcb0c8cfeb1a5148fef15d
lucassfgh <Lucas.SCHNABEL@fgh.team>
2021-09-03 06:29:32 +0200

    add coinlist

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, redirect, url_for
+from pycoingecko import CoinGeckoAPI
+from pymongo import MongoClient
+import time
+
+app = Flask(__name__)
+
+cg = CoinGeckoAPI()
+clientMongo = MongoClient('mongodb://localhost:27017')
+db = clientMongo.crypto
+
+coinListCollection = db.coinList
+
+
+@app.route("/")
+def index():
+    return redirect(url_for('ping'))
+
+
+@app.route("/ping")
+def ping():
+    return cg.ping()
+
+
+@app.route("/coins/list", methods=['GET'])
+def printCoinsList():
+    return str(coinListCollection.find())
+
+
+@app.route("/coins/list", methods=['POST'])
+def updateCoinsList():
+    for i in cg.get_coins_list():
+        coinListCollection.insert_one(i)
+    return redirect(url_for("/coins/list"))
+
+
+
